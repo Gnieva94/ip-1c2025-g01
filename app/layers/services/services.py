83dda7b08017acc9ee78394fd355346e1dd9cbcd
JOAQUIN <jarceguillem@gmail.com>
2025-06-17 20:15:46 -0300

    def filterbytype

--- app/layers/services/services.py
+++ app/layers/services/services.py
@@ -36,10 +36,12 @@ def filterByCharacter(name):
 # función que filtra las cards según su tipo.
 def filterByType(type_filter):
     filtered_cards = []
+    print(type_filter)
 
     for card in getAllImages():
         # debe verificar si la casa de la card coincide con la recibida por parámetro. Si es así, se añade al listado de filtered_cards.
-        filtered_cards.append(card)
+        if card.types and type_filter.lower() in card.types:
+            filtered_cards.append(card)
 
     return filtered_cards
 
